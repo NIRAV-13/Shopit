9818ef25b3862880966af9b29708a9131a90ab91
Sherrin Raji <sh810841@dal.ca>
2022-06-10 22:27:02 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World!
+        </p>
+        <a
+          className="App-link"
+          href="https://git.cs.dal.ca/nirav/csci_5709_advance_web_group_20/-/tree/main"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Link to my individual repository
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
