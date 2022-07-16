eafe622706af88145724e4db2b1e396928adb9c9
Sherrin Raji <sh810841@dal.ca>
2022-07-15 23:20:14 -0300

    order change

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ var cookieParser = require('cookie-parser');
 var mongoose = require('mongoose');
 var cors = require("cors")
 const cartRouter = require('./routes/cartRouter');
+const orderRouter = require('./routes/orderRouter');
 const PORT = process.env.PORT || 8080;
 var mongodb ='mongodb+srv://nirav:Assignment3!@cluster0.dmey5jx.mongodb.net/shopit?retryWrites=true&w=majority';
 mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('database connection successfully'));
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/cart',cartRouter);
+app.use("/", orderRouter);
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
   });
