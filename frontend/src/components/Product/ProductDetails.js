87fbf1763c63a70a0f28f3d601f0c65b2b23af42
Lavita Pereira <lv795165@dal.ca>
2022-07-16 06:07:58 -0300

    changes to product

--- frontend/src/components/Product/ProductDetails.js
+++ frontend/src/components/Product/ProductDetails.js
@@ -1,91 +1,94 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import NavBar from '../NavBar/NavBar';
-import ProductArray from './ProductArray';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../NavBar/NavBar";
+import ProductArray from "./ProductArray";
 import { useNavigate } from "react-router-dom";
-import './ProductCss.css'
-import axios from 'axios';
+import "./ProductCss.css";
+import axios from "axios";
 
 const ProductDetails = () => {
-    const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
-    let navigate = useNavigate();
-    const { id } = useParams();
+  let navigate = useNavigate();
+  const { id } = useParams();
 
-    // const filtered = data.filter(prod => {
-    //     return prod.id == id;
-    // });
+  // const filtered = data.filter(prod => {
+  //     return prod.id == id;
+  // });
 
-    const [isHovering, setIsHovering] = useState(false);
+  const [isHovering, setIsHovering] = useState(false);
 
-    const handleMouseEnter = () => {
-        setIsHovering(true);
-    };
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
 
-    const handleMouseLeave = () => {
-        setIsHovering(false);
-    };
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+  };
 
+  const routeChange = () => {
+    let path = `/wishlist`;
+    navigate(path);
+  };
 
-    const routeChange = () => {
-        let path = `/wishlist`;
-        navigate(path);
-    }
+  // console.log(filtered);
 
-    // console.log(filtered);
+  const fetchProduct = async () => {
+    let res = await axios({
+      method: "GET",
+      url: "http://localhost:8080/fetchProductByProductID/" + id,
+    });
 
-    const fetchProduct = async () => {
-        let res = await axios({
-            method: "GET",
-            url: "http://localhost:8080/fetchProductByProductID/" + id
-        });
+    console.log(res.data);
+    setData(res.data);
+  };
 
-        console.log(res.data);
-        setData(res.data)
-    };
+  useEffect(() => {
+    fetchProduct();
+  }, []);
 
-    useEffect(() => {
-        fetchProduct()
-    }, [])
+  return (
+    <div>
+      {data.map((prod, idx) => {
+        return (
+          <>
+            <NavBar></NavBar>
+            <div className="details" key={idx}>
+              <div className="big-img">
+                {/* <img src={"http://localhost:3000/" + prod.img} className="img-fluid" alt="..." />  */}
+                <img src={prod.productImage} className="img-fluid" alt="..." />
+              </div>
 
-    return (
-        <div>
-            {data.map((prod, idx) => {
-                return (
-                    <>
-                        <NavBar></NavBar>
-                        <div className='details' key={idx}>
-                            <div className='big-img'>
-                                {/* <img src={"http://localhost:3000/" + prod.img} className="img-fluid" alt="..." />  */}
-                                <img src={prod.productImage} className="img-fluid" alt="..." />
-                            </div>
+              <div className="box">
+                <div className="row">
+                  <h1>{prod.productBrand}</h1>
+                  <h5>{prod.productName}</h5>
+                  <span>${prod.productPrice}</span>
+                </div>
+                <p>{prod.productDescription}</p>
+                <div className="buttonRow">
+                  <button
+                    className="cart"
+                    style={{
+                      backgroundColor: isHovering ? "pink" : "",
+                      color: isHovering ? "white" : "",
+                    }}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                  >
+                    {" "}
+                    <span class="bi bi-handbag-fill"></span> Add to cart
+                  </button>
 
-                            <div className='box'>
-                                <div className='row'>
-                                    <h1>{prod.productBrand}</h1>
-                                    <h5>{prod.productName}</h5>
-                                    <span>${prod.productPrice}</span>
-                                </div>
-                                <p>{prod.desc}</p>
-                                <div className='buttonRow'>
-                                    <button className="cart" style={{
-                                        backgroundColor: isHovering ? 'pink' : '',
-                                        color: isHovering ? 'white' : '',
-                                    }}
-                                        onMouseEnter={handleMouseEnter}
-                                        onMouseLeave={handleMouseLeave}> <span class='bi bi-handbag-fill'></span> Add to cart</button>
+                  {/* <button className="wishlist"> <span class='bi bi-suit-heart'></span> Add to cart</button> */}
+                </div>
+              </div>
+            </div>
+          </>
+        );
+      })}
+    </div>
+  );
+};
 
-                                    {/* <button className="wishlist"> <span class='bi bi-suit-heart'></span> Add to cart</button> */}
-                                </div>
-                            </div>
-
-                        </div>
-                    </>
-                );
-            })}
-        </div>
-
-    );
-}
-
-export default ProductDetails
+export default ProductDetails;
