3b134e735d94e91baf8df1fd255dccf70bab6c65
Shivam Rank <rankshivam23@gmail.com>
2022-07-16 00:17:34 -0300

    Lists Done

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const orderController = require("../controllers/orderController");
-var router = express.Router();
+const admin = require("../controllers/admin")
+const router = express.Router();
 
 router.get("/order/get", orderController.getOrders);
+router.get("/admin", admin.list);
+router.post("/delete", admin.delete_user);
+//router.post("/view", admin.view_user);
 module.exports = router;
